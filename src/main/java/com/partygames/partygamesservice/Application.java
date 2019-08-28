f0a5eb8c2ee46090522edb7eda3296a316a2b388
Matthew Long <long.matthew29@gmail.com>
2019-08-28 18:40:14 -0400

    added generated spring boot project

---
+++ src/main/java/com/partygames/partygamesservice/Application.java
@@ -0,0 +1,13 @@
+package com.partygames.partygamesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
