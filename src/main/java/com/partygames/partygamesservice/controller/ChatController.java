3953ba816c4cb547f95813e3d1c1a945b48ce552
Matthew Long <long.matthew29@gmail.com>
2019-10-05 18:09:01 -0400

    initial simple chat impl

--- src/main/java/com/partygames/partygamesservice/controller/ChatController.java
+++ src/main/java/com/partygames/partygamesservice/controller/ChatController.java
@@ -1,45 +1,59 @@
 package com.partygames.partygamesservice.controller;
 
-import com.partygames.partygamesservice.model.Message;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
+  private final SimpMessagingTemplate template;
+
+  @Autowired
+  ChatController(SimpMessagingTemplate template) {
+    this.template = template;
+  }
+
+  @MessageMapping("/send/message")
+  public void onReceivedMessage(String message) {
+    this.template.convertAndSend("/chat", new SimpleDateFormat("HH:mm:ss").format(new Date()) + " - " + message);
+  }
+
   /**
    * sendMessage.
    */
-  @MessageMapping("/chat.sendMessage")
-  @SendTo("/topic/public") // TODO make dynamic rooms?
-  public Message sendMessage(@Payload Message chatMessage) {
-    return chatMessage;
-  }
+  // @MessageMapping("/chat.sendMessage")
+  // @SendTo("/topic/public") // TODO make dynamic rooms?
+  // public Message sendMessage(@Payload Message chatMessage) {
+  // return chatMessage;
+  // }
 
   /**
    * addUser: adds a user to the instance.
    */
-  @MessageMapping("/chat.addUser")
-  @SendTo("/topic/public") // TODO make dynamic rooms?
-  public Message addUser(@Payload Message chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-    // add username in web socket session
-    headerAccessor.getSessionAttributes().put("author", chatMessage.getSender());
+  // @MessageMapping("/chat.addUser")
+  // @SendTo("/topic/public") // TODO make dynamic rooms?
+  // public Message addUser(@Payload Message chatMessage,
+  // SimpMessageHeaderAccessor headerAccessor) {
+  // // add username in web socket session
+  // headerAccessor.getSessionAttributes().put("author", chatMessage.getSender());
 
-    return chatMessage;
-  }
+  // return chatMessage;
+  // }
 
   /**
    * removeUser: adds a user to the instance.
    */
-  @MessageMapping("/chat.removeUser")
-  @SendTo("/topic/public") // TODO make dynamic rooms?
-  public Message removeUser(@Payload Message chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-    // Add username in web socket session
-    headerAccessor.getSessionAttributes().put("author", chatMessage.getSender());
+  // @MessageMapping("/chat.removeUser")
+  // @SendTo("/topic/public") // TODO make dynamic rooms?
+  // public Message removeUser(@Payload Message chatMessage,
+  // SimpMessageHeaderAccessor headerAccessor) {
+  // // Add username in web socket session
+  // headerAccessor.getSessionAttributes().put("author", chatMessage.getSender());
 
-    return chatMessage;
-  }
+  // return chatMessage;
+  // }
 }
