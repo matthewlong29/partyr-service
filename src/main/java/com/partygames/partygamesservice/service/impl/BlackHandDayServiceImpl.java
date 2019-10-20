dccb1291852421d10790802073032ffe48e822b4
Matthew Long <long.matthew29@gmail.com>
2019-10-20 17:22:15 -0400

    added blackhand day and night services

---
+++ src/main/java/com/partygames/partygamesservice/service/impl/BlackHandDayServiceImpl.java
@@ -0,0 +1,7 @@
+package com.partygames.partygamesservice.service.impl;
+
+import com.partygames.partygamesservice.service.BlackHandDayService;
+
+public class BlackHandDayServiceImpl implements BlackHandDayService {
+
+}
---
+++ src/main/java/com/partygames/partygamesservice/service/impl/BlackHandNightServiceImpl.java
@@ -0,0 +1,7 @@
+package com.partygames.partygamesservice.service.impl;
+
+import com.partygames.partygamesservice.service.BlackHandNightService;
+
+public class BlackHandNightServiceImpl implements BlackHandNightService {
+
+}
