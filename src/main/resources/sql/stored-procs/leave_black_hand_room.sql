ecd47fdfe894630840571a06a916f2944dae801f
Matthew Long <long.matthew29@gmail.com>
2019-11-04 20:09:49 -0500

    added leave and delete room functionality

---
+++ src/main/resources/sql/stored-procs/leave_black_hand_room.sql
@@ -0,0 +1,30 @@
+USE `partyrdb`;
+DROP procedure IF EXISTS `leave_black_hand_room`;
+
+DELIMITER $$
+USE `partyrdb`$$
+CREATE DEFINER=`root`@`%` PROCEDURE `leave_black_hand_room`(
+  IN i_room_name VARCHAR(32),
+  IN i_email VARCHAR(32)
+)
+BEGIN
+  DECLARE hostEmail VARCHAR(32);
+  DECLARE newHostEmail VARCHAR(32);
+  DECLARE numOfPlayers int;
+
+  DELETE FROM `partyrdb`.`black_hand_rooms` WHERE 
+	`game_room_name` = i_room_name AND `email` = i_email;
+    
+  SELECT `host_email` INTO hostEmail FROM `partyrdb`.`lobby` WHERE `game_room_name` = i_room_name;
+  SELECT `number_of_players` INTO numOfPlayers FROM `partyrdb`.`lobby` WHERE `game_room_name` = i_room_name;
+    
+  IF numOfPlayers = 1 THEN
+    DELETE FROM `partyrdb`.`lobby` WHERE `game_room_name` = i_room_name;
+  ELSEIF hostEmail = i_email THEN
+    SELECT `email` INTO newHostEmail FROM `partyrdb`.`black_hand_rooms` WHERE `game_room_name` = i_room_name limit 1;
+	UPDATE `partyrdb`.`lobby` SET `number_of_players` = `number_of_players` - 1, `host_email` = newHostEmail WHERE
+      `game_room_name` = i_room_name;
+  END IF;
+END$$
+
+DELIMITER ;
